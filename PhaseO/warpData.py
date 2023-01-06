0576312f47e360e0f375f2ec3b7b985aed22b30a
Devesh Datwani <datwanidevesh@gmail.com>
2023-01-05 23:09:28 -0500

    data generated

--- PhaseO/warpData.py
+++ PhaseO/warpData.py
@@ -2,35 +2,55 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import os
+from PIL import Image
+import json
+
+global labels 
+labels = dict()
 
 def createImageSet(imagePath):
+    global labels
     image = cv2.imread(imagePath)
-    image = cv2.resize(image, (1000,1000))
-    topLeft = np.array([400,400])
-    bottomLeft = np.array([800,400])
-    topRight = np.array([topLeft[0], topLeft[1]+400])
-    bottomRight = np.array([bottomLeft[0], bottomLeft[1]+400])
+    #image = cv2.resize(image, (1000,1000))
+    topLeft = np.array([100,50])
+    bottomLeft = np.array([200,50])
+    topRight = np.array([topLeft[0], topLeft[1]+100])
+    bottomRight = np.array([bottomLeft[0], bottomLeft[1]+100])
     
-    R1 = np.random.randint(-100,100)
-    R2 = np.random.randint(-100,100)
-    R3 = np.random.randint(-100,100) 
-    R4 = np.random.randint(-100,100)
+    R1 = np.random.randint(-20,20)
+    R2 = np.random.randint(-20,20)
+    R3 = np.random.randint(-20,20) 
+    R4 = np.random.randint(-20,20)
 
-    Image1 = image[topLeft[1]:topRight[1], bottomLeft[1]:bottomRight[1], :]
+    Image1 = image[topLeft[0]:bottomLeft[0], topLeft[1]:topRight[1], :]
     newtopLeft = topLeft + R1
     newbottomLeft = bottomLeft + R2
     newtopRight = topRight + R3
     newbottomRight = bottomRight + R4
     transformation = cv2.getPerspectiveTransform(np.float32([topLeft, topRight, bottomLeft, bottomRight]), np.float32([newtopLeft, newtopRight, newbottomLeft, newbottomRight]))
     transformedImage = cv2.warpPerspective(image, transformation, dsize=(image.shape[1], image.shape[0]))
-    Image2 = transformedImage[topLeft[1]:topRight[1], bottomLeft[1]:bottomRight[1], :]
-    dataInstance = np.dstack(Image1, Image2)
-    cv2.imwrite(dataInstance, "something")
-    #Write lagel
+    Image2 = transformedImage[topLeft[0]:bottomLeft[0], topLeft[1]:topRight[1], :]
+    
+    dataPathOne = "/home/deveshdatwani/Stitching/PhaseO/Train/Images/viewOne/" + imagePath.split("/")[7] 
+    dataPathTwo = "/home/deveshdatwani/Stitching/PhaseO/Train/Images/viewTwo/" + imagePath.split("/")[7]
+
+    try:
+        cv2.imwrite(dataPathOne, Image1)
+        cv2.imwrite(dataPathTwo, Image2)
+        print(imagePath)
+    except Exception as e:
+        pass
+
+    labels[imagePath] = tuple(map(tuple, transformation))
 
     return None
 
 if __name__ == "__main__":
-    for i in os.listdir():
-        imagePath = i
-        createImageSet(imagePath) 
+    basePath = "/home/deveshdatwani/Stitching/PhaseO/Train/val2017/"
+    for i in os.listdir("/home/deveshdatwani/Stitching/PhaseO/Train/val2017"):
+        imagePath = basePath + i
+        createImageSet(imagePath)
+
+    with open('/home/deveshdatwani/Stitching/PhaseO/Train/Labels/trainlabel.json', 'w') as fp:
+        json.dump(labels, fp)
+     
