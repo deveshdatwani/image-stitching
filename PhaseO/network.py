818e0d8264ed7ff1b1769c0222c811571344f072
Devesh Datwani <datwanidevesh@gmail.com>
2023-01-11 11:40:53 -0500

    dataset + basic forward

--- PhaseO/network.py
+++ PhaseO/network.py
@@ -17,41 +17,41 @@ class HomographyNet(nn.Module):
         self.fullyConnected1 = nn.Linear(16*16*128, 1024, bias=True)
         self.fullyConnected2 = nn.Linear(1024, 8)
 
-        def forward(self, x):
-            x = F.relu(self.conv1(x))
-            x = F.relu(self.conv2(x))
-            x = self.max(x)
-            x = F.relu(self.conv3(x))
-            x = F.relu(self.conv4(x))
-            x = self.max(x)
-            x = F.relu(self.conv5(x))
-            x = F.relu(self.conv6(x))
-            x = self.max(x)
-            x = F.relu(self.conv7(x))
-            x = F.relu(self.conv8(x))
-            x = x.reshape(-1, 16*16*128)
-            x = F.relu(self.fullyConnected1(x))
-            x = self.fullyConnected2(x)
-            
-            return x
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.max(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.max(x)
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.max(x)
+        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv8(x))
+        x = x.reshape(-1, 16*16*128)
+        x = F.relu(self.fullyConnected1(x))
+        x = self.fullyConnected2(x)
+        
+        return x
 
-        def lossFunction(self, x):
-            return None
+    def lossFunction(self, x):
+        return None
 
-        def trainStep(self, x, trainLoader, epochs=10, batchSize=8):
-            optimizer = self.optimizer
-            trainSize = len(trainLoader)
-            for epoch in range(epochs):
-                for batchIdx, (data,target) in enumerate(trainLoader):
-                    optimizer.zero_grad()
-                    out = self.model(data)
-                    loss = nn.MSELoss(target, out)
-                    loss.backward()
-                    optimizer.step()
-                    if batchIdx & 20 == 0:
-                        print(f'Train epoch {epoch} iteration {batchIdx} / {trainSize}')                    
+    def trainStep(self, x, trainLoader, epochs=10, batchSize=8):
+        optimizer = self.optimizer
+        trainSize = len(trainLoader)
+        for epoch in range(epochs):
+            for batchIdx, (data,target) in enumerate(trainLoader):
+                optimizer.zero_grad()
+                out = self.model(data)
+                loss = nn.MSELoss(target, out)
+                loss.backward()
+                optimizer.step()
+                if batchIdx & 20 == 0:
+                    print(f'Train epoch {epoch} iteration {batchIdx} / {trainSize}')                    
 
-            return None
+        return None
 
-        def validateStep(self):
-            return None
+    def validateStep(self):
+        return None
