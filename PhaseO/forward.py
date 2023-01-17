fe5240c71df554b4e079f9633c05c41b3d9876a4
Devesh Datwani <datwanidevesh@gmail.com>
2023-01-16 21:29:14 -0500

    images stacked

--- PhaseO/forward.py
+++ PhaseO/forward.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import torch
 from torchvision.io import read_image
+from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     model = HomographyNet()
@@ -11,5 +12,7 @@ if __name__ == "__main__":
     path2 = "/home/deveshdatwani/Stitching/PhaseO/Train/Images/viewTwo/"
     image1 = read_image(path1 + filename)
     image2 = read_image(path2 + filename)
-    IMAGE = torch.stack((image1, image2))
-    output = model(image1.float())  
+    IMAGE = torch.stack((image1, image2)).reshape((6,100,100))
+    output = model(IMAGE.float())
+    print(output)
+    
